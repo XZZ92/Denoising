79f24038336f6235eb9acce5b6690ee024f0fbd5
Sara Bizjak <sarabizjak97@gmail.com>
2017-09-07 12:07:40 +0200

    dodala funkcijo za izracun okolice

--- main.py
+++ main.py
@@ -6,13 +6,40 @@ import imageio
 # OPIS POSTOPKA
 # Preberem ime vhodne in izhodne slike
 # Odprem vhodno sliko kot numpy array4
-# Naredi kopijo vhodne slike
+# Naredim kopijo vhodne slike
 # Za vsak piksel p:
-#  - * Izracunaj njegovo k-"okolico" B_k(p) --- shranis jo v nek seznam (ali numpy array)
-#  - * Izracunaj mediano elementov tega seznama m
-#  - V novi sliki piksel p prepisi z mediano elementov njegove k-okolice m
-# Shrani novo sliko z ustreznim imenom
+#  - * Izracunam njegovo k-"okolico" B_k(p) --- shranim jo v nek seznam (ali numpy array)
+#  - * Izracunam mediano elementov tega seznama m
+#  - V novi sliki piksel p prepisem z mediano elementov njegove k-okolice m
+# Shranim novo sliko z ustreznim imenom
 
+
+def get_neighborhood(I, i, j, k):
+	
+	w, h, _ = I.shape
+	
+	seznam_okolice = []
+	
+	for idx in range(i - k, i + k):
+		for jdx in range(j - k, j + k):
+			#kdaj smo izven slike
+			
+			if idx < 0 or idx > w - 1 or jdx < 0 or jdx > h - 1:
+				continue
+			
+			seznam_okolice.append((idx, jdx))
+			
+	return seznam_okolice	
+	
+	
+# def get_geometric_median(I, ???): 
+
+	# for i in range(w):
+		# for j in range(h):
+			# okolica = get_neighborhod(I, i, j, k)
+			
+			
+	
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
 		print("Uporaba: python main.py vhodna izhodna")
@@ -23,37 +50,31 @@ if __name__ == '__main__':
 
 	w, h, _ = I.shape
 	
-	# seznam_rgb = []
 	
 	for i in range(w):
 		for j in range(h):
-			# okolica = get_neighborhod(I, i, j, 3)
-			print((i, j), " -> ", I[i, j])
-			#seznam_rgb.append(I[i, j])
+			okolica = get_neighborhood(I, i, j, 3)
+			print(okolica)
+			#print((i, j), " -> ", I[i, j])
+			#mediana = get_geometric_median(???)
+			
 			
 	
 	print("Dimenzije slike: ", I.shape)
+	
 
 	
 	
-def get_neighborhod(I, i, j, k):
 	
-	infile = sys.argv[1]
-	I = imageio.imread(infile)
-	
-	w, h, _ = I.shape
-	
-	sez = []
-   
-    for idx in range(i - k, i + k):
-	    for jdx in range(j - k, j + k):
-			#kdaj smo izven slike
-			if idx == i and jdx == j:
-				continue
-			if i < 0 or i > h - 1 and j < 0 or j > w - 1:
-				continue
+
 			
-			
-            sez.append(I[idx, jdx])
+
+	
+
+	
+	
+	
+
+	
 			
 			
