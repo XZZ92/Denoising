a70cc498606f19c2edd26243281f8f08c83bf23f
Sara Bizjak <sarabizjak97@gmail.com>
2017-09-11 22:11:31 +0200

    koncan program

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sys
 import numpy
 import imageio
 
-import copy
 
 # OPIS POSTOPKA
 # Preberem ime vhodne in izhodne slike
@@ -38,7 +37,7 @@ def get_neighborhood(I, i, j, k):
 def get_rgb_neighborhood(I, i, j):
 	'''izracuna k-okolica prostora (r, g, b) na mestu piksla (i, j)'''
 	
-	okolica1 = get_neighborhood(I, i, j, 2)
+	okolica1 = get_neighborhood(I, i, j, 1)
 	rgb_okolica = []
 	
 	for element in okolica1:
@@ -48,10 +47,8 @@ def get_rgb_neighborhood(I, i, j):
 	return rgb_okolica
 		
 
-			
-def get_geometric_median(I, i, j):
-	'''izracuna mediano rgb-okolice na mestu piksla (i, j) - mediana izracunana posebej za vsako rgb-komponento'''
-
+def get_median(I, i, j):
+	'''izracuna povprecje rgb-okolice na mestu piksla (i, j) - posebej za vsako rgb-komponento'''
 
 	r_okolica = []
 	g_okolica = []
@@ -62,38 +59,79 @@ def get_geometric_median(I, i, j):
 		r_okolica.append(element[0])
 		g_okolica.append(element[1])
 		b_okolica.append(element[2])
-		
-	r_okolica.sort()
-	g_okolica.sort()
-	b_okolica.sort()
+	
 	
 	r = len(r_okolica)
 	g = len(g_okolica)
 	b = len(b_okolica)
 	
-	if r % 2 == 0:
-		prvi = r // 2 - 1
-		drugi = r // 2
-		r_average = float((int(r_okolica[prvi]) + int(r_okolica[drugi])) / 2)
-	else:
-		r_average = float(r_okolica[r // 2])
-		
-	if g % 2 == 0:
-		prvi = g // 2 - 1
-		drugi = g // 2
-		g_average = float((int(g_okolica[prvi]) + int(g_okolica[drugi])) / 2)
-	else:
-		g_average = float(g_okolica[g // 2])
-		
-	if b % 2 == 0:
-		prvi = b // 2 - 1
-		drugi = b // 2
-		b_average = float((int(b_okolica[prvi]) + int(b_okolica[drugi])) / 2)
-	else:
-		b_average = float(b_okolica[b // 2])
-		
-		
+	r_sum = 0
+	g_sum = 0
+	b_sum = 0
+	
+	for element in r_okolica:
+		r_sum += element
+	
+	r_average = r_sum / r
+	
+	for element in g_okolica:
+		g_sum += element
+	
+	g_average = g_sum / g
+	
+	for element in b_okolica:
+		b_sum += element
+	
+	b_average = b_sum / b
+	
 	return [r_average, g_average, b_average]
+	
+	
+# def get_median(I, i, j):
+	# '''izracuna mediano rgb-okolice na mestu piksla (i, j) - mediana izracunana posebej za vsako rgb-komponento'''
+
+
+	# r_okolica = []
+	# g_okolica = []
+	# b_okolica = []
+	
+	# okolica2 = get_rgb_neighborhood(I, i, j)
+	# for element in okolica2:
+		# r_okolica.append(element[0])
+		# g_okolica.append(element[1])
+		# b_okolica.append(element[2])
+		
+	# r_okolica.sort()
+	# g_okolica.sort()
+	# b_okolica.sort()
+	
+	# r = len(r_okolica)
+	# g = len(g_okolica)
+	# b = len(b_okolica)
+	
+	# if r % 2 == 0:
+		# prvi = r // 2 - 1
+		# drugi = r // 2
+		# r_average = float((int(r_okolica[prvi]) + int(r_okolica[drugi])) / 2)
+	# else:
+		# r_average = float(r_okolica[r // 2])
+		
+	# if g % 2 == 0:
+		# prvi = g // 2 - 1
+		# drugi = g // 2
+		# g_average = float((int(g_okolica[prvi]) + int(g_okolica[drugi])) / 2)
+	# else:
+		# g_average = float(g_okolica[g // 2])
+		
+	# if b % 2 == 0:
+		# prvi = b // 2 - 1
+		# drugi = b // 2
+		# b_average = float((int(b_okolica[prvi]) + int(b_okolica[drugi])) / 2)
+	# else:
+		# b_average = float(b_okolica[b // 2])
+		
+		
+	# return [r_average, g_average, b_average]
 
 	
 	
@@ -117,9 +155,11 @@ if __name__ == '__main__':
 			#print(okolica)
 			#rgb = get_rgb_neighborhood(I, i, j)
 			#print(rgb)
-			mediana = get_geometric_median(I, i, j)
+			mediana = get_median(I, i, j)
 			
 			J[i, j] = mediana
+			
+			print("Progress: " + str(round(i/w*100, 1)) + "%", end="\r")
 			
 	imageio.imwrite(outfile, J)	
 			
